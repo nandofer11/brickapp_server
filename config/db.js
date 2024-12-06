27f2c9c5318bd4d6dbec2f0b2dcf5865ff863820
Orlando Fernández <nandofer.11@hotmail.com>
2024-12-06 00:33:02 -0500

    [UPDT] db.js para manejar errores

--- config/db.js
+++ config/db.js
@@ -1,22 +1,38 @@
-// db.js
 const mysql = require('mysql');
 require('dotenv').config();
 
-const db = mysql.createConnection({
-    user: process.env.DB_USER || 'root',
-    host: process.env.DB_HOST || 'localhost',
-    password: process.env.DB_PASSWORD || 'root',
-    database: process.env.DB_NAME || 'bdbrickapp',
-    port: process.env.DB_PORT || 3306,
-    connectTimeout: 10000,
-});
+let db;
 
-db.connect((err) => {
-    if (err) {
-        console.error('Error conectando a la base de datos:', err);
-    } else {
-        console.log('Conexión a la base de datos exitosa');
-    }
-});
+function connectDatabase() {
+    db = mysql.createConnection({
+        user: process.env.DB_USER || 'root',
+        host: process.env.DB_HOST || 'localhost',
+        password: process.env.DB_PASSWORD || 'root',
+        database: process.env.DB_NAME || 'bdbrickapp',
+        port: process.env.DB_PORT || 3306,
+        connectTimeout: 10000,
+    });
+
+    db.connect((err) => {
+        if (err) {
+            console.error('Error conectando a la base de datos:', err);
+            setTimeout(connectDatabase, 5000); // Intenta reconectar después de 5 segundos
+        } else {
+            console.log('Conexión a la base de datos exitosa');
+        }
+    });
+
+    db.on('error', (err) => {
+        console.error('Error en la conexión de la base de datos:', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            connectDatabase(); // Reconecta en caso de pérdida de conexión
+        } else {
+            throw err;
+        }
+    });
+}
+
+connectDatabase();
 
 module.exports = db;
+
